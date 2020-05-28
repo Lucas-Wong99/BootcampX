deae0d1def0241392b2647811cdbc4e15ec55af7
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-28 02:40:52 +0000

    Added safety from SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,17 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const limit = process.argv[3];
-const cohort = process.argv[2]
-
-pool.query(
-`
+const query = `
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`; 
+
+const limit = process.argv[3] || 5;
+const cohort = process.argv[2];
+
+pool.query(query, [`%${cohort}%`, limit])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,24 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const month = process.argv[2]
+const query = `
+SELECT distinct teachers.name AS name, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests
+ON teachers.id = teacher_id
+JOIN students
+ON assistance_requests.student_id = students.id
+JOIN cohorts
+ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY name;
+`;
 
-pool.query(
-  `
-  SELECT distinct teachers.name AS name, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests
-  ON teachers.id = teacher_id
-  JOIN students
-  ON assistance_requests.student_id = students.id
-  JOIN cohorts
-  ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${month}'
-  ORDER BY name;
-  `
-)
+const cohort = process.argv[2];
+
+pool.query(query, [`${cohort}`])
 .then((res) => {
-  res.rows.forEach(item => {
-    console.log(`${item.cohort}: ${item.name}`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.name}`);
   })
 }).catch(err => console.error('query error', err.stack));
